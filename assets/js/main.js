d802bafc4f5fe8fc9344d8799f9367065f85b943
João Paulo Canário <jopacanario@gmail.com>
2017-03-08 21:35:09 -0300

    Fixed indentation tab
    Created a event listener for click on tab leaderboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,60 @@
 function loadJSON(file, callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', file, true);
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', file, true);
 
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-        }
-    };
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
 
-    xobj.send(null);
+  xobj.send(null);
 }
 
 function generateLeaderboard(file) {
-    loadJSON(file, function(json) {
-        var athletes = JSON.parse(json);
-        var pos = 1, next = 1, prev = 0;
+  loadJSON(file, function(json) {
+    var athletes = JSON.parse(json);
+    var pos = 1, next = 1, prev = 0;
 
-        var leaderboard = document.getElementById('leaderboard-content');
-        leaderboard.innerHTML = "";
+    var leaderboard = document.getElementById('leaderboard-content');
+    leaderboard.innerHTML = "";
 
-        athletes.forEach(athlete => {
-            if (prev != athlete.overallScore) {
-               pos = next;
-            }
+    athletes.forEach(athlete => {
+      if (prev != athlete.overallScore) {
+        pos = next;
+      }
 
-            prev = athlete.overallScore;
-            next++;
+      prev = athlete.overallScore;
+      next++;
 
-            var elem = `
-              <tr>
-                <td>${pos}</td>
-                <td class="mdl-data-table__cell--non-numeric">${athlete.name}</td>
-                <td>${athlete.affiliate}</td>
-                <td>${athlete.overallScore}</td>
-                <td>${athlete.wod1Display} (${athlete.wod1Rank})</td>
-                <td>${athlete.wod2Display} (${athlete.wod2Rank})</td>
-                <td>--(--)</td>
-                <td>--(--)</td>
-                <td>--(--)</td>
-              </tr>
-            `;
+      var elem = `
+        <tr>
+          <td>${pos}</td>
+          <td class="mdl-data-table__cell--non-numeric">${athlete.name}</td>
+          <td>${athlete.affiliate}</td>
+          <td>${athlete.overallScore}</td>
+          <td>${athlete.wod1Display} (${athlete.wod1Rank})</td>
+          <td>${athlete.wod2Display} (${athlete.wod2Rank})</td>
+          <td>--(--)</td>
+          <td>--(--)</td>
+          <td>--(--)</td>
+        </tr>
+      `;
 
-            leaderboard.innerHTML += elem;
-        });
+      leaderboard.innerHTML += elem;
     });
+  });
 }
 
 generateLeaderboard("assets/data/women_leaderboard.json");
+
+var men = document.getElementById('menLeaderboard');
+men.addEventListener('click', () => {
+  generateLeaderboard("assets/data/men_leaderboard.json");
+});
+
+var women = document.getElementById('womenLeaderboard');
+women.addEventListener('click', () => {
+  generateLeaderboard("assets/data/women_leaderboard.json");
+});
